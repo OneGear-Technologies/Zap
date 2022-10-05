119333e62bca6e8b2882c215292a018beffc4074
ru2saig <42857541+ru2saig@users.noreply.github.com>
2022-10-05 12:24:08 +0530

    light mode/dark mode implementation

--- App.tsx
+++ App.tsx
@@ -3,25 +3,47 @@ import {
   StyleSheet,
   Text,
   View,
+  useColorScheme,
 } from 'react-native';
 
 
 const App = () => {
+  const colorScheme = useColorScheme();
+
+  const textTheme = colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText
+  const containerTheme = colorScheme === 'light' ? styles.lightContainer : styles.darkContainer
 
   return (
-    <View style={styles.container}>
-      <Text>Hello, there!</Text>
+    <View style={[styles.container, containerTheme]}>
+      <Text style={[styles.text, textTheme]}>Colour Theme: { colorScheme } </Text>
     </View>
   );
 };
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    jusifyContents: 'center'
+  },
+
+  text: {
     
-    backgroundColor: '#fff'
   },
   
+  lightContainer: {
+    backgroundColor: '#d0d0c0',
+  },
+  darkContainer: {
+    backgroundColor: '#161719',
+  },
+  lightThemeText: {
+    color: '#242c40',
+  },
+  darkThemeText: {
+    color: '#d0d0c0',
+  },
 });
 
 export default App;

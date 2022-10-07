b219b9f186405f47fbe8faeda8cc900eb62d2f8e
ru2saig <42857541+ru2saig@users.noreply.github.com>
2022-10-07 12:45:23 +0530

    Scans QR codes, and displays the value

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,21 +1,53 @@
+import { useState, useEffect } from 'react'
 import {
   Text,
   View,
   useColorScheme,
+  StyleSheet,
 } from 'react-native';
 import { globalStyles } from '../styles/GlobalStyles'
+import { useCameraDevices, Camera } from 'react-native-vision-camera'
+import { useScanBarcodes, BarcodeFormat } from 'vision-camera-code-scanner'
 
 
 const HomeScreen = () => {
   const colorScheme = useColorScheme();
-
   const textTheme = colorScheme === 'light' ? globalStyles.lightThemeText : globalStyles.darkThemeText
   const containerTheme = colorScheme === 'light' ? globalStyles.lightContainer : globalStyles.darkContainer
 
+  const [hasPermission, setHasPermission] = useState(false);
+
+  const devices = useCameraDevices()
+  const device = devices.back
+
+  const [frameProcessor, qrcodeScanned] = useScanBarcodes([BarcodeFormat.QR_CODE], {
+    checkInverted: true,
+  });
+
+  useEffect(() => {
+    (async () => {
+      const status = await Camera.requestCameraPermission();
+      setHasPermission(status === 'authorized');
+    })();
+  }, []);
+
+  
+  if (device == null) return <View />
   return (
-    <View style={[globalStyles.container, containerTheme]}>
-      <Text style={[globalStyles.text, textTheme]}>Home: { colorScheme } </Text>
-    </View>
+    <>
+    <Camera
+      style={StyleSheet.absoluteFill}
+      device={device}
+      isActive={true}
+    frameProcessor={frameProcessor}
+    frameProcessorFps={5}
+    />
+    {qrcodeScanned.map((barcode, idx) => (
+      <Text key={idx} style={globalStyles.darkThemeText}>
+        {barcode.displayValue}
+      </Text>
+    ))}
+    </>
   );
 }
 
